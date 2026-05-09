b4e0407bb2da9fa432d01c83d9a751c51ba2ef9f
Mohd Zuhaib Khan <zuhaibkhan1915691@gmail.com>
2026-05-10 00:58:37 +0530

    Production settings update

--- config/config/settings.py
+++ config/config/settings.py
@@ -15,9 +15,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-secret-key")
 
-DEBUG = os.getenv("DJANGO_DEBUG", "True") == "True"
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "leadflow-crm-5z22.onrender.com",
+]
 
 
 # -------------------------------------------------------------------
