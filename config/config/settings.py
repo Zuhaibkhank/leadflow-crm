f4fb77d9c46001b7c22af89e453fb8ea6618410b
Mohd Zuhaib Khan <zuhaibkhan1915691@gmail.com>
2026-05-10 16:13:22 +0530

    configured render databse

--- config/config/settings.py
+++ config/config/settings.py
@@ -104,15 +104,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # DATABASE CONFIGURATION (PostgreSQL)
 # -------------------------------------------------------------------
 
+import dj_database_url
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME", "leadflow_crm"),
-        "USER": os.getenv("DB_USER", "postgres"),
-        "PASSWORD": os.getenv("DB_PASSWORD", "postgres123"),
-        "HOST": os.getenv("DB_HOST", "localhost"),
-        "PORT": os.getenv("DB_PORT", "5432"),
-    }
+    'default': dj_database_url.parse(
+        os.getenv("DATABASE_URL")
+    )
 }
 
 
